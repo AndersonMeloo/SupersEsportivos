2faf870025ff54e3fec19d30dd1a7727c66a70df
Anderson Melo <142186594+AndersonMeloo@users.noreply.github.com>
2025-08-16 18:50:05 -0300

    adicionando interface Props no Controller CreateCar

--- backend/src/controllers/CreateCarService.ts
+++ backend/src/controllers/CreateCarService.ts
@@ -1,13 +1,23 @@
 import { FastifyRequest, FastifyReply } from "fastify";
 import { CreateCarService } from "../services/CreateCarService";
 
+interface CreateCarControllerProps{ 
+    modelo: string,
+     marca: string,
+      imagem: string, 
+      descricao: string,
+      anoKm: string,
+       local: string, 
+       preco: number 
+    }
+
 class CreateCarController {
 
     async handle(request: FastifyRequest, reply: FastifyReply) {
 
-        const { marca, modelo, imagem, descricao, anoKm, local, preco } = request.body as { modelo: string, marca: string, imagem: string, descricao: string, anoKm: string, local: string, preco: number }
+        const { marca, modelo, imagem, descricao, anoKm, local, preco } = request.body as CreateCarControllerProps
 
-        console.log(marca, modelo, imagem, descricao, anoKm, local, preco)
+            console.log(marca, modelo, imagem, descricao, anoKm, local, preco)
 
         const customerCar = new CreateCarService()
 
